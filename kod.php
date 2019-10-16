8e2ff9d43fb935621ee5afdba0e21cd603a9faae
hunsvpa <50731760+hunsvpa@users.noreply.github.com>
2019-10-16 19:37:25 +0200

    Create kod.php

---
+++ kod.php
@@ -0,0 +1,10 @@
+// Ez egy bemutató projekt a verzió kezeléshez
+// a fájl neve: kod.php és ez egy PHP fájl lesz, webalkalmazáshoz
+
+<?php
+function Kiirni($mitIrjonKi) {
+return $mitIrjonKi;
+}
+
+echo Kiirni("Helló Eurasia IT");
+?>
